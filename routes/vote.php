b2364b2a6c1350343d5b333aed04b44a005825f8
prudentgps <40212194+prudentgps@users.noreply.github.com>
2020-11-16 16:26:31 +0100

    Update vote.php

--- routes/vote.php
+++ routes/vote.php
@@ -13,7 +13,7 @@
 Route::group(['middleware' => ['auth']], function () {
 
     Route::resource('user', 'UserController');
-    Route::get('get_matric', 'UserController@check');
+    Route::post('get_matric', 'UserController@check');
 
     Route::resource('election', 'ElectionController');
 
