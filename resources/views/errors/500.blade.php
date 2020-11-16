5ada98f61949804c1cd8e8c3d8411ab1bf8b767e
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-11-16 10:36:55 +0100

    500

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,31 @@
-@extends('errors::minimal')
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>500 Internal Error</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/500.css') }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+<body>
 
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+<h5>Internal Server error !</h5>
+<h1>5</h1>
+<h1>00</h1>
+<div class="box">
+    <span></span><span></span>
+    <span></span><span></span>
+    <span></span>
+</div>
+<div class="box">
+    <span></span><span></span>
+    <span></span><span></span>
+    <span></span>
+</div>
+<p> We're unable to find out what's happening! We suggest you to
+    <br/>
+    <a href="{{ route('dashboard') }}">Go Back</a>
+    or visit here later.</p>
+</body>
+</html>
