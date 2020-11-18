88a509701e0d1e941c71fbcc06170faa314322ac
prudentgps <40212194+prudentgps@users.noreply.github.com>
2020-11-18 14:49:41 +0100

    Update sec.blade.php

--- resources/views/vote/backend/sec.blade.php
+++ resources/views/vote/backend/sec.blade.php
@@ -20,13 +20,15 @@
                             @csrf
 
                             <div class="dropdown">
+                                <div class="input-group input-group-alternative">
                                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="em em-question" aria-role="presentation" aria-label="BLACK QUESTION MARK ORNAMENT"></i> Regular
+                                    <i class="em em-question" aria-role="presentation" aria-label="BLACK QUESTION MARK ORNAMENT"></i>                            Regular
                                 </button>
                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                     <a class="dropdown-item" href="#">Action</a>
                                     <a class="dropdown-item" href="#">Another action</a>
                                     <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
                                 </div>
                             </div>
 
