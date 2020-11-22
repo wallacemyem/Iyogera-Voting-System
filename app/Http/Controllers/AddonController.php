65cf23c110d92e6ecc8016ed18f2f918c292689e
Wallace Aboiyar <32508088+wallacemyem@users.noreply.github.com>
2020-11-22 20:33:06 +0100

    Update AddonController.php

--- app/Http/Controllers/AddonController.php
+++ app/Http/Controllers/AddonController.php
@@ -14,7 +14,7 @@ class AddonController extends Controller
 {
 
     public function onstart(){
-        
+        if ( Auth::check()){
         Auth::logout(); 
 
         session(['role' => '']); 
@@ -22,6 +22,8 @@ class AddonController extends Controller
         Session::flush();
 
         return view('vote.backend.vue');
+            }else{
+            return view('vote.backend.vue');
     }
 
     public function vote(Request $request)
